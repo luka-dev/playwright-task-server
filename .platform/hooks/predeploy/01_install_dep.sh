7d15d49fb33cc89f59dacc0b92f2a8695d694347
luka <leandr1999@gmail.com>
2020-09-04 12:08:23 +0300

    platform update

--- .platform/hooks/predeploy/01_install_dep.sh
+++ .platform/hooks/predeploy/01_install_dep.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-sudo yum install -y cups-libs dbus-glib libXrandr libXcursor libXinerama cairo cairo-gobject pango
+sudo yum install -y alsa-lib.x86_64 atk.x86_64 cups-libs.x86_64 gtk3.x86_64 ipa-gothic-fonts libXcomposite.x86_64 libXcursor.x86_64 libXdamage.x86_64 libXext.x86_64 libXi.x86_64 libXrandr.x86_64 libXScrnSaver.x86_64 libXtst.x86_64 pango.x86_64 xorg-x11-fonts-100dpi xorg-x11-fonts-75dpi xorg-x11-fonts-cyrillic xorg-x11-fonts-misc xorg-x11-fonts-Type1 xorg-x11-utils
+sudo yum update nss -y
