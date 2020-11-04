ac62a29d2abb5e33dafa954136c68bfbbfa0823b
luka <leandr1999@gmail.com>
2020-11-04 22:18:50 +0200

    fix: AWS swap

---
+++ .platform/hooks/predeploy/01_make_swap.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+SWAPFILE=/var/swapfile
+SWAP_BLOCKS=126
+SWAP_BLOCK_MEGABYTES=128M
+
+if [ -f $SWAPFILE ]; then
+	echo "Swapfile $SWAPFILE found, assuming already setup"
+	exit;
+fi
+
+#SWAP_BLOCKS * $SWAP_BLOCK_MEGABYTES = 10GB
+/bin/dd if=/dev/zero of=$SWAPFILE bs=$SWAP_BLOCK_MEGABYTES count=$SWAP_BLOCKS
+/bin/chmod 600 $SWAPFILE
+/sbin/mkswap $SWAPFILE
+/sbin/swapon $SWAPFILE
