eebced2ee50a55a9b32b9fa8673ee0a92e584c3a
luka <leandr1999@gmail.com>
2021-02-23 11:11:16 +0200

    fix: workaround known bug with dialogs https://github.com/microsoft/playwright/issues/4179

--- src/Context.ts
+++ src/Context.ts
@@ -1,9 +1,11 @@
 import {BrowserContext} from "playwright-core";
+import {Dialog} from "playwright-chromium";
 
 export default class Context {
 
     public readonly initTime: number;
     private browserContext: BrowserContext | null = null;
+    private dialogs: Dialog[] = [];
 
     public constructor(initTime: number | null = null, browserContext: BrowserContext | null = null) {
         if (initTime === null) {
@@ -16,10 +18,27 @@ export default class Context {
 
     public setBrowserContext(browserContext: BrowserContext): void {
         this.browserContext = browserContext;
+
+        //workaround for known bug https://github.com/microsoft/playwright/issues/4179
+        this.browserContext.on('page', page => {
+            page.on('dialog', dialog => {
+                this.dialogs.push(dialog);
+            });
+        })
     }
 
     public async closeContext() {
         if (this.browserContext !== null) {
+
+            for (const dialog of this.dialogs) {
+                try {
+                    await dialog.dismiss();
+                }
+                catch (e) {
+                    //if dialog already doesnt exist
+                }
+            }
+
             await this.browserContext.close()
             return true;
         }
